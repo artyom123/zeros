42bd320d5ee6e9a0e03a4c58b75cedd76dc210c0
Your Name <you@example.com>
2018-02-23 00:00:17 +0300

    zeros decided

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let count = 0, i = 1, x = 0;
+
+    while(x < number){
+        x = Math.pow(5,i++);
+        count += Math.floor(number / x);
+    }
+    return count;
 }
